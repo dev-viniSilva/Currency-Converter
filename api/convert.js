4544ae776f5a914793a8039c54e9b20c086cfe38
Vini silva <vinrun123@gmail.com>
2026-03-08 13:03:41 -0400

    second commit

--- api/convert.js
+++ api/convert.js
@@ -1,28 +1,32 @@
-import fetch from 'node-fetch'
+import 'dotenv/config';
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
     try {
-        const { from, to, amount } = req.query
+        const { from, to, amount } = req.query;
 
-        // URL da API externa com sua chave do .env
-        const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${from}`
-
-        const response = await fetch(API_URL)
-        if (!response.ok) return res.status(500).json({ error: "Erro na API externa" })
-
-        const data = await response.json()
-
-        if (!data.conversion_rates || !data.conversion_rates[to]) {
-            return res.status(500).json({ error: "Moeda inválida" })
+        if (!from || !to || !amount) {
+            return res.status(400).json({ error: "Parâmetros faltando" });
         }
 
-        const rate = data.conversion_rates[to]
-        const cleanAmount = Number(amount.toString().replace(/ /g, '').replace(/,/g, ''))
-        const result = cleanAmount * rate
+        const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${from}`;
 
-        res.status(200).json({ result, rate })
+        const response = await fetch(API_URL);
+        if (!response.ok) return res.status(500).json({ error: "Erro na API externa" });
+
+        const data = await response.json();
+
+        if (!data.conversion_rates || !data.conversion_rates[to]) {
+            return res.status(500).json({ error: "Moeda inválida" });
+        }
+
+        const rate = data.conversion_rates[to];
+        const cleanAmount = Number(amount.toString().replace(/ /g, '').replace(/,/g, ''));
+        const result = cleanAmount * rate;
+
+        res.status(200).json({ result, rate });
     } catch (err) {
-        console.error(err)
-        res.status(500).json({ error: "Erro interno no servidor" })
+        console.error(err);
+        res.status(500).json({ error: "Erro interno no servidor" });
     }
 }
